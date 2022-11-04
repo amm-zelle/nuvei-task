8caac60d0b32d4d01279171415ca42627bcab55b
Afam <fammymba@gmail.com>
2022-11-04 12:01:57 -0600

    updated

---
+++ aws-api/README.md
@@ -0,0 +1,4 @@
+Before you can run this script:
+AWS CLI must be installed
+AWS CLI must be configured
+The profile you use must have the necessary permisions to perform the operations in the script
---
+++ aws-api/aws-resource-resolver.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Simple Shell Script to list all the resources being used in account per region
+# Pre-requisite: 
+# You must have the AWS CLI installed and configured
+mapfile -t my_array < <(aws ec2 describe-regions --output text | awk '{print $4}')# Retrieves all the existing AWS Region
+declare -p my_array # Store the regions as an array
+# For loop to list all the resources region by region 
+for i in "${my_array[@]}"
+do
+   
+  
+   echo -e '\nListing Resources in region: '$i''
+
+   aws resourcegroupstaggingapi get-resources --region $i | grep ResourceARN
+done
